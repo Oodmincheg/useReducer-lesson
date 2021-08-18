d5b99dd8a46fb62db1d3dccd018b2377aa1d5aa1
Vorobiov Vladyslav <vorobyov.vladislav@gmail.com>
2021-08-18 21:01:55 +0300

    final verion with custom hook

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,10 +1,18 @@
-import React, {useState} from 'react'
+import React from 'react';
+import useCounter from './useCounter'
 
-export default function Counter({initialCount = 0}) {
-    const [count, setCount] = useState(initialCount)
-    return (
-        <button onClick={() => setCount(count + 1)}>
-            {count}
-        </button>
-    )
+
+export default function Counter() {
+  const [state, dispatch] = useCounter()
+
+  return (
+    <>
+      <div>Count: {state.count}</div>
+      <div>DoubleCount: {state.doubleCount}</div>
+      <button onClick={() => dispatch({ type: 'increment' })}>+</button>
+      <button onClick={() => dispatch({ type: 'decrement' })}>-</button>
+      <button onClick={() => dispatch({ type: 'doubleIncrement' })}>+2</button>
+      <button onClick={() => dispatch({ type: 'doubleDecrement' })}>-2</button>
+    </>
+  );
 }
---
+++ src/components/useCounter.js
@@ -0,0 +1,32 @@
+import {useEffect, useReducer} from 'react' 
+
+   const  initialCount = { count: 0, doubleCount: 0 }
+
+   const reducerFunction = (state, action) => {
+    switch (action.type) {
+      case 'increment':
+        return { ...state, count: state.count + 1 };
+      case 'decrement':
+        if (state === 0) return state;
+        return { ...state, count: state.count - 1 };
+      case 'doubleIncrement':
+        return { ...state, doubleCount: state.doubleCount + 2 };
+      case 'doubleDecrement':
+        return { ...state, doubleCount: state.doubleCount - 2 };
+      default:
+        return state;
+    }
+  };
+
+function useCounter() {
+    
+const [state, dispatch] = useReducer(reducerFunction, initialCount);
+  
+  useEffect(() => {
+     console.log(state.count)
+  }, [state.count])
+
+  return [state, dispatch]
+}
+
+export default useCounter;
